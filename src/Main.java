b1e6e9f953113921ae368ee0156c3d82096493b5
Aurora <auduong.301@gmail.com>
2025-09-07 19:42:22 +0700

    fist initialize

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class Main {
+   public static void main(String[] args) {
+      Stack<String> stack = new Stack<>();
+
+      stack.push("Genshin Impact");
+      stack.push("Honkai Star Rail");
+      stack.push("Star Dew Valley");
+      stack.push("Final Fantasy VII");
+   }
+}
